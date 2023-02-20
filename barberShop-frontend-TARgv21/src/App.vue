5272c6fc5d287b859a84775229edd2a2d9c2d8d4
veta05 <jelizaveta05@gmail.com>
2023-02-20 17:10:20 +0200

    add barber details

--- barberShop-frontend-TARgv21/src/App.vue
+++ barberShop-frontend-TARgv21/src/App.vue
@@ -1,47 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <barber-details :barber="barber"></barber-details>
 </template>
+<script>
+const API_URL = "http://localhost:8080/barbers"
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+import barberDetails from "./components/barberDetails.vue"
+    export default{
+        components:{
+            barberDetails
+        },
+        data(){
+            return{
+                id: 1,
+                barber: {
+                    name: "Marko Tasane",
+                    bookingDate: "2023-02-15",
+                    bookingTime: "10:00:00"
+                }
+            }
+        },
+        created(){
+            this.fetchData()
+        },
+        methods:{
+            async fetchData(){
+                const url = `${API_URL}/${this.id}`
+                this.barber = await (await fetch(url)).json()
+            }
+        }
+    }
+</script>
---
+++ barberShop-frontend-TARgv21/src/components/barberDetails.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Name: {{ name }}</h1>
+        <p>Booking date: {{ bookingDate }}</p>
+        <p>Booking time: {{ bookingTime }}</p>
+    </div>
+</template>
+<script>
+    export default{
+        props: {
+            barber: Object,
+            required: true
+        },
+        data(){
+            return {
+                name: this.barber.name,
+                bookingDate: this.barber.bookingDate,
+                bookingTime: this.barber.bookingTime
+            }
+        }
+    }
+</script>
